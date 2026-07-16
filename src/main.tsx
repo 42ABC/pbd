54b98680827eea8bda4b32e003dde866e5673d3a
Andrew Brady <acbrady@andrew.cmu.edu>
2026-07-16 14:39:26 -0400

    bug fix

--- src/main.tsx
+++ src/main.tsx
@@ -36,7 +36,7 @@ if (!rootElement) {
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
       {ReactQueryDevtools && (
